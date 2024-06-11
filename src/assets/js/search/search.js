8ce60f389322966f32a6710b6ec6403c1b09fa01
Linlccc <linlccc@163.com>
2024-06-11 23:36:13 +0800

    完成搜索js

--- src/assets/js/search/search.js
+++ src/assets/js/search/search.js
@@ -1,11 +1,14 @@
 // 参数引入 see:https://gohugo.io/hugo-pipes/js/
+// 这里得到的参数名全是小写
 import { fuseOptions as confFuseOptions } from "@params";
 
 // 搜索引擎 see: https://www.fusejs.io/
 let fuse;
+const El_Input = document.getElementById("searchInput");
+const El_Result = document.getElementById("searchResults");
 
 // fuse.js 默认搜索选项
-const defaultFuseOptions = {
+const fuseOptions = {
   // 是否区分大小写
   isCaseSensitive: false,
   // 是否包含分数
@@ -27,8 +30,8 @@ const defaultFuseOptions = {
   // 使用扩展搜索
   useExtendedSearch: false,
   // 忽略位置
-  ignoreLocation: false,
-  // 忽略场范数
+  ignoreLocation: true,
+  // 忽略字段规范
   ignoreFieldNorm: false,
   // 字段标准权重
   fieldNormWeight: 1,
@@ -36,7 +39,10 @@ const defaultFuseOptions = {
   keys: ["title", "permalink", "summary", "content"],
 };
 // 融合默认选项和配置选项
-const fuseOptions = { ...defaultFuseOptions, ...confFuseOptions };
+for (const key in fuseOptions) {
+  const lowerKey = key.toLowerCase();
+  lowerKey in confFuseOptions && (fuseOptions[key] = confFuseOptions[lowerKey]);
+}
 
 // 请求搜索数据
 fetch("../index.json")
@@ -50,3 +56,65 @@ fetch("../index.json")
 window.test = (a) => {
   return fuse.search(a);
 };
+
+// 防抖
+function debounce(fn, delay) {
+  let timer;
+  return function (...args) {
+    timer && clearTimeout(timer);
+    timer = setTimeout(() => fn.apply(this, args), delay);
+  };
+}
+// 搜索框输入
+El_Input.addEventListener(
+  "input",
+  debounce(() => {
+    console.log(1);
+    if (!fuse) return;
+    const result = fuse.search(El_Input.value.trim());
+    if (result.length == 0) {
+      El_Result.innerHTML = "";
+      return;
+    }
+    // 结果html列表
+    let resultSet = "";
+    for (const item of result) resultSet += `<li><a class="fuseResult" href="${item.item.permalink}">${item.item.title}&nbsp;»</a></li>`;
+    El_Result.innerHTML = resultSet;
+  }, 200),
+);
+// × 点击
+El_Input.addEventListener("search", () => {
+  El_Input.value = "";
+});
+
+// 按键监听
+window.addEventListener("keydown", (e) => {
+  if (El_Result.childNodes.length == 0) return;
+  const ae = document.activeElement;
+  let focusEl = null;
+  switch (e.key) {
+    // 向下
+    case "ArrowDown":
+      e.preventDefault();
+      if (ae == El_Input || !ae.classList.contains("fuseResult")) focusEl = El_Result.firstChild.lastChild;
+      else focusEl = ae.parentElement.nextSibling.lastChild;
+      break;
+    // 向上
+    case "ArrowUp":
+      e.preventDefault();
+      if (ae == El_Input || ae.parentElement == El_Result.firstChild) focusEl = El_Input;
+      else focusEl = ae.parentElement.previousSibling.lastChild;
+      break;
+    // 向右
+    case "ArrowRight":
+      ae.click();
+      break;
+    case "Escape":
+      El_Input.value = "";
+      El_Input.dispatchEvent(new Event("input"));
+      break;
+    default:
+      break;
+  }
+  focusEl?.focus();
+});
